7647f5921f28d6ff47252d9c20e5df836c5c4a73
Mohammed-0Alamri <162879150+Mohammed-0Alamri@users.noreply.github.com>
2024-03-13 10:44:37 +0000

    python dictionary inside html

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ from flask import Flask,render_template
 
 
 app = Flask(__name__)
+projects = [
+    {
+        'id': 1,
+        'project': 'Web Scraping',
+        'skills': 'urllib python, Python Requests, Selenium, Beautiful Soup'
+    },
+    {
+        'id': 2,
+        'project': 'AI model for flare monitoring',
+        'skills': 'Python pandas, scikit-learn, matplotlib, Microsoft Azure, pychatgpt'
+    }
+]
 
 @app.route("/")
 
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",projects=projects,)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
