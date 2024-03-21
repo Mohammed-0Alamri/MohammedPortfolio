e5374b225822fec3fe919c9a5a6579bbdd6352fc
Mohammed-0Alamri <162879150+Mohammed-0Alamri@users.noreply.github.com>
2024-03-21 08:34:46 +0000

    build contact me, and details button

--- database.py
+++ database.py
@@ -1,7 +1,29 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
+import os
 
-engine = create_engine("mysql+pymysql://admin:1234admin@database-1.crm0ksuia7ig.eu-north-1.rds.amazonaws.com/projects?charset=utf8mb4")
+engine = create_engine(os.environ['DB_CONNECTION_STRING'])
 
-with engine.connect() as conn:
-    result = conn.execute(text("select * from trial"))
-    print(result.all())
+
+def load_projects_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from Myprojects"))
+    projects = []
+    for row in result.all():
+      projects.append(dict(row._mapping))
+  return projects
+
+
+def load_project_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from Myprojects where id = {id}"))
+    rows = result.all()
+    return dict(rows[0]._mapping)
+
+def add_message_to_db(data):
+  with engine.connect() as conn:
+    sql = text(
+        f"INSERT INTO mymessages (Full_name, email, Massage) VALUES (\'{data['full_name']}\', \'{data['email']}\', \'{data['message']}\')"
+    )
+    print(sql)
+    conn.execute(sql)
+    conn.commit()
